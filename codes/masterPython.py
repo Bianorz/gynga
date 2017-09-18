4b6f1b5ed01d6d3b796539a39243d98ed3ee4f64
deoqc <deo.queiroz.carlos@gmail.com>
2017-09-18 11:01:25 -0300

    tasks for week 3

--- codes/masterPython.py
+++ codes/masterPython.py
@@ -3,9 +3,11 @@ import smbus
 import time
 import sys
 import struct
- 
+
+
 bus = smbus.SMBus(1)
 address = 0x05
+
 
 def ints_to_float(numbers):
     """
@@ -13,21 +15,29 @@ def ints_to_float(numbers):
     >>> ints_to_float([63, 128, 0, 0])
     1.0
     """
-    return struct.unpack('<f', struct.pack('4B', *numbers))[0]  
+    return struct.unpack('<f', struct.pack('4B', *numbers))[0]
+
 
 def main():
     gpio.setmode(gpio.BCM)
     gpio.setup(17, gpio.OUT)
-    status = False
-    dataRec = [0,0,0,0]
-    while 1:
-        gpio.output(17, status)
-	bus.write_i2c_block_data(address, 0b0 , [0b1, 0b10,0b11])
-	time.sleep(1)
-	for i in range(0,4):
-	    dataRec[i] = bus.read_byte(address)
-	n_float = ints_to_float(dataRec)
-	print("%.2f" % n_float)
+
+    while True:
+        bus.write_i2c_block_data(address, 0b0, [0b1, 0b10, 0b11])
+        time.sleep(1)
+
+        # TASK 1: test with list compreension
+        dataRec1 = [bus.read_byte(address) for _ in range(4)]
+        # TASK 2: test with read block data
+        dataRec2 = read_i2c_block_data(address, 0, 4)
+
+        n_float1 = ints_to_float(dataRec1)
+        n_float2 = ints_to_float(dataRec2)
+
+        print("%.2f" % n_float1)
+        print("%.2f" % n_float2)
+
+
 if __name__ == '__main__':
     try:
         main()
--- codes/slaveAtMega.c
+++ codes/slaveAtMega.c
@@ -6,6 +6,7 @@
 #define BUFFSIZE 0x04
 #define SLAVEADDRESS 0X05
 
+// TASK: add comment about `union` (c++) vs `typedef union` (c)
 typedef union{
   float v;
   uint8_t b[4];
@@ -16,20 +17,27 @@ uint8_t dataRead[BUFFSIZE] = {0}; // buff onde será armazenado o dado recebido
 //uint8_t dataWrite[BUFFSIZE] = {0}; // buff onde será armazenado o dado enviado
 uint8_t i=0,j=0; // contador
 
+
+// TASK: document ISR, e.g. its use cases, sources to read about it
+// document also TWI_vect
 ISR(TWI_vect) {
 	static unsigned char i2c_state;
 	unsigned char twi_status;
 
 	// Disable Global Interrupt
+	// TASK: why exactly is it needed? what could happen if not used?
 	cli();
 
 	// Get TWI Status Register, mask the prescaler bits (TWPS1,TWPS0)
+	// only the 5 firsts bits are used for twi_status (0xF8 = b11111000)
+	// TASK: explain the twi_status (in the context of TWI)
+	//
+	// TASK: explain in detail each case
 	twi_status = TWSR & 0xF8;
    switch (twi_status) {
-	case TW_SR_SLA_ACK:      // 0x60: SLA+W received, ACK returned
-		//i2c_state = 0;          // Start I2C State for Register Address required
-		TWCR |= (1 << TWINT);    // Clear TWINT Flag
-		break;
+	// case TW_SR_SLA_ACK:      // 0x60: SLA+W received, ACK returned
+	// 	//i2c_state = 0;          // Start I2C State for Register Address required
+	// 	break;
 
 	case TW_SR_DATA_ACK:     // 0x80: data received, ACK returned
 		//PORTB = TWDR;
@@ -37,33 +45,28 @@ ISR(TWI_vect) {
 			dataRead[i] = TWDR;
 			i = i + 1;
 		}
-
-		TWCR |= (1 << TWINT);    // Clear TWINT Flag
 		break;
 
 	case TW_SR_STOP: // 0xA0: stop or repeated start condition received while selected
 		i = 0;
-		j=0;
-		TWCR |= (1 << TWINT);    // Clear TWINT Flag
+		j = 0;
 		break;
 
 	case TW_ST_SLA_ACK:      // 0xA8: SLA+R received, ACK returned
+  // TASK: explain! could it be commented out?
 	case TW_ST_DATA_ACK:     // 0xB8: data transmitted, ACK received
-		if (j < BUFFSIZE){
-		TWDR = var.b[j];      // Store data in TWDR register
-		j = j +1;
+		if (j < BUFFSIZE) {
+		  TWDR = var.b[j];      // Store data in TWDR register
+		  j = j +1;
 		}
 		//i2c_state = 0;	      // Reset I2C State
-		TWCR |= (1 << TWINT);    // Clear TWINT Flag
 		break;
 
-	case TW_ST_DATA_NACK:    // 0xC0: data transmitted, NACK received
-	case TW_ST_LAST_DATA:    // 0xC8: last data byte transmitted, ACK received
-	case TW_BUS_ERROR:       // 0x00: illegal start or stop condition
-	default:
-		TWCR |= (1 << TWINT);    // Clear TWINT Flag
-		//i2c_state = 0;         // Back to the Begining State
-	}
+	// case TW_ST_DATA_NACK:    // 0xC0: data transmitted, NACK received
+	// case TW_ST_LAST_DATA:    // 0xC8: last data byte transmitted, ACK received
+	// case TW_BUS_ERROR:       // 0x00: illegal start or stop condition
+
+  TWCR |= (1 << TWINT);
 
 	// Enable Global Interrupt
 	sei();
@@ -74,10 +77,13 @@ void enableI2c(uint8_t slave_address) {
 	uint8_t gen_call = 0; // gen_call desabilitada
 	TWAR = ((slave_address << 1) | gen_call << 1);
 	TWDR = 0x00;                 // Default Initial Value
+  // TASK: explain the use of each register
 	TWCR = (1 << TWINT) | (1 << TWEA) | (1 << TWEN) | (1 << TWIE);
 	sei();
 }
 
+
+// TASK: explain the logical flow necessary to run with i2c/twi
 int main(void) {
 	var.v = 5.2;
 	DDRB = 0xFF;      // Set PORTB: PB0=Input, Others as Output
